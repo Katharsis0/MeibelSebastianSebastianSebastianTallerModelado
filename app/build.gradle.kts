229c606c42d1e625fb3080220a44088c627e038d
Sebastián Hidalgo Vargas <55753352+Katharsis0@users.noreply.github.com>
2025-01-30 00:12:57 -0600

    UnitTestsCompleted

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ dependencies {
     implementation ("com.google.android.gms:play-services-auth:21.3.0")
     implementation("com.google.android.gms:play-services-maps:19.0.0")
     implementation(libs.play.services.maps)
+    testImplementation ("org.mockito:mockito-core:5.3.1")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
